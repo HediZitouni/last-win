5b48b99b26dfd67a72f163428e3d63b33ea0ce49
HediZitouni <zitouni.hedi1@gmail.com>
2022-09-08 00:19:19 +0200

    remove log + some lint

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,10 +1,11 @@
 const HTTP_METHODS = {
-  GET: "GET",
-  POST: "POST",
-  PUT: "PUT",
-  DELETE: "DELETE",
+	GET: 'GET',
+	POST: 'POST',
+	PUT: 'PUT',
+	DELETE: 'DELETE',
 };
 
-const API_ADRESS = "https://otrom.fr/back/";
+const API_ADRESS = 'https://otrom.fr/back/';
+//const API_ADRESS = 'http://localhost:3000/';
 
 export { HTTP_METHODS, API_ADRESS };
