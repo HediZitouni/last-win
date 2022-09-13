0dbadffddfcfcb55b008a58b93a19474aafcf94a
HediZitouni <zitouni.hedi1@gmail.com>
2022-09-13 21:50:25 +0200

    add refresh button for dashboard

--- components/board/board.tsx
+++ components/board/board.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
-import { background_grey, last_green } from '../../utils/common-styles';
+import { View, Text, StyleSheet, ScrollView, Button } from 'react-native';
+import { background_grey, button_grey, last_green } from '../../utils/common-styles';
 import { getUsers } from '../users/users.service';
 import { User } from '../users/users.type';
 
 const Board = () => {
 	const [users, setUsers] = React.useState<User[]>([]);
+	const [triggerRefresh, setTriggerRefresh] = React.useState<boolean>(false);
+
 	useEffect(() => {
 		getUsers()
 			.then((users) => {
@@ -15,30 +17,36 @@ const Board = () => {
 			.catch((error) => {
 				console.log(error);
 			});
-	}, []);
+	}, [triggerRefresh]);
+
 	return (
-		<View style={styles.board_container}>
-			<ScrollView>
-				{users.map((user, index) => {
-					return (
-						<View style={[styles.board_item, user.isLast ? styles.board_item_last : null]} key={index}>
-							<View style={styles.board_item_name}>
-								<Text>{user.name}</Text>
+		<>
+			<View style={styles.board_container}>
+				<ScrollView>
+					{users.map((user, index) => {
+						return (
+							<View style={[styles.board_item, user.isLast ? styles.board_item_last : null]} key={index}>
+								<View style={styles.board_item_name}>
+									<Text>{user.name}</Text>
+								</View>
+								<View style={styles.board_item_score}>
+									<Text>{user.score}</Text>
+								</View>
 							</View>
-							<View style={styles.board_item_score}>
-								<Text>{user.score}</Text>
-							</View>
-						</View>
-					);
-				})}
-			</ScrollView>
-		</View>
+						);
+					})}
+				</ScrollView>
+			</View>
+			<View style={styles.refresh_button_container}>
+				<Button title="Refresh board" color={button_grey} onPress={() => setTriggerRefresh((prev) => !prev)} />
+			</View>
+		</>
 	);
 };
 
 const styles = StyleSheet.create({
 	board_container: {
-		flex: 9,
+		flex: 8,
 		backgroundColor: background_grey,
 	},
 	board_item: {
@@ -61,6 +69,12 @@ const styles = StyleSheet.create({
 	},
 	board_item_score: {
 		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	refresh_button_container: {
+		flex: 1,
+		backgroundColor: background_grey,
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
