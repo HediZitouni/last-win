b4ff57fce656e26da220e05bbf04de69cd26967b
hedi <zitouni.hedi1@gmail.com>
2023-08-07 12:34:03 +0200

    change url ws

--- store/store.ts
+++ store/store.ts
@@ -14,5 +14,5 @@ export default configureStore({
     user: usersSlice,
     game: gameSlice,
   },
-  enhancers: [applyMiddleware(websocketMiddleware("ws://localhost:3000/"))],
+  enhancers: [applyMiddleware(websocketMiddleware("ws://otrom.fr/back:3000/"))],
 });
